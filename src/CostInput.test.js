76db2c1464bf674c2e6055b5d80fa4cb35e0ebf7
Alex <alex@wendtedesigns.com>
2018-09-12 13:03:46 -0500

    initial commit.

---
+++ src/CostInput.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, cleanup, fireEvent } from 'react-testing-library';
+import CostInput from './CostInput';
+
+afterEach(cleanup);
+it('handleChange function called', () => {
+  const spy = jest.fn();
+  const { getByLabelText } = render(<CostInput handleChange={spy} />);
+  let input = getByLabelText('Item Cost');
+  expect(input.value).toBe('');
+  fireEvent.change(input, { target: { value: '23' } });
+  expect(spy).toHaveBeenCalledTimes(1);
+});
