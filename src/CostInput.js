d1471656f18b66c188943d340b1c8e3c9c3fb7da
Alex <alex@wendtedesigns.com>
2018-09-26 11:13:29 -0500

    Implements solution. Installs correct eslint and prettier files.

--- src/CostInput.js
+++ src/CostInput.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
 const CostInput = ({ handleChange }) => (
   <div>
     <label htmlFor="cost">Item Cost</label>
-    <input type="string" id="cost" onChange={handleChange} />
+    <input type="text" id="cost" onChange={handleChange} />
   </div>
-);
-export default CostInput;
+)
+export default CostInput
--- src/CostInput.test.js
+++ src/CostInput.test.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { render, cleanup, fireEvent } from 'react-testing-library';
-import CostInput from './CostInput';
+import React from 'react'
+import { render, cleanup, fireEvent } from 'react-testing-library'
+import CostInput from './CostInput'
 
-afterEach(cleanup);
+afterEach(cleanup)
 it('handleChange function called', () => {
-  const spy = jest.fn();
-  const { getByLabelText } = render(<CostInput handleChange={spy} />);
-  let input = getByLabelText('Item Cost');
-  expect(input.value).toBe('');
-  fireEvent.change(input, { target: { value: '23' } });
-  expect(spy).toHaveBeenCalledTimes(1);
-});
+  const spy = jest.fn()
+  const { getByLabelText } = render(<CostInput handleChange={spy} />)
+  const input = getByLabelText('Item Cost')
+  expect(input.value).toBe('')
+  fireEvent.change(input, { target: { value: '23' } })
+  expect(spy).toHaveBeenCalledTimes(1)
+})
